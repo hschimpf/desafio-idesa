7aca5a7ddfdfb050b8272154427b759775d8bf84
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2022-02-22 17:42:43 -0300

    Enabled /countries endpoint

--- src/com/example/project/webservice/api/v1_0/WebserviceAPI.class.php
+++ src/com/example/project/webservice/api/v1_0/WebserviceAPI.class.php
@@ -5,15 +5,18 @@
 
     require_once __DIR__.'/endpoints/LoginEndpoint.class.php';
     require_once __DIR__.'/endpoints/UsersEndpoint.class.php';
+    require_once __DIR__.'/endpoints/CountriesEndpoint.class.php';
 
     use com\example\project\webservice\api\AbstractWebserviceAPI;
 
     use com\example\project\webservice\api\v1_0\endpoints\LoginEndpoint;
     use com\example\project\webservice\api\v1_0\endpoints\UsersEndpoint;
+    use com\example\project\webservice\api\v1_0\endpoints\CountriesEndpoint;
 
     class WebserviceAPI extends AbstractWebserviceAPI {
         public function login() { return new LoginEndpoint($this); }
         public function users() { return new UsersEndpoint($this); }
+        public function countries() { return new CountriesEndpoint($this); }
 
         private function isCurrentUser($user) {
             //
