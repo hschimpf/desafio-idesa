0fc0d423c62cacf9fd4ff5b29e5a12d3a56a24bd
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2022-02-22 17:52:10 -0300

    Added /clients endpoint and WS console condif

--- src/com/example/project/webservice/api/v2_0/WebserviceAPI.class.php
+++ src/com/example/project/webservice/api/v2_0/WebserviceAPI.class.php
@@ -4,11 +4,14 @@
     require_once __DIR__.'/../v1_0/WebserviceAPI.class.php';
 
     require_once __DIR__.'/endpoints/UsersEndpoint.class.php';
+    require_once __DIR__.'/endpoints/ClientsEndpoint.class.php';
 
     use com\example\project\webservice\api\v1_0\WebserviceAPI as WebserviceAPI_v1_0;
 
     use com\example\project\webservice\api\v2_0\endpoints\UsersEndpoint;
+    use com\example\project\webservice\api\v2_0\endpoints\ClientsEndpoint;
 
     class WebserviceAPI extends WebserviceAPI_v1_0 {
         public function users() { return new UsersEndpoint($this); }
+        public function clients() { return new ClientsEndpoint($this); }
     }
